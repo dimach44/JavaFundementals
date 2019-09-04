70d7be777c9c8e3707833e7d3d6aed7929c65cbf
dimach44 <dimach44@gmail.com>
2019-09-04 17:00:59 -0400

    Operators

--- src/com/revature/intro/ControlStatements.java
+++ src/com/revature/intro/ControlStatements.java
@@ -3,7 +3,11 @@ package com.revature.intro;
 public class ControlStatements {
 	
 	public static void main(String[] args) {
+		intToHex(90);
 		
+	}
+	
+	static void intToHex(int n) {
 		int num = 90;
 		String out = "";
 		
@@ -19,6 +23,24 @@ public class ControlStatements {
 		}
 		
 		System.out.println(out);
-		
+	}
+	
+	static int leftShift(int num, int shift) {
+		return num << shift;
+	}
+	
+	static boolean relational(Object obj) {
+		if(obj instanceof String) return true;
+		else return false;
+	}
+	
+	public boolean equalTo(int a, int b) {
+		if(a == b) return true;
+		else if (a != b) return false;
+		else return false;
+	}
+	
+	static int bitwiseOr(int a, int b) {
+		return a | b;
 	}
 }
