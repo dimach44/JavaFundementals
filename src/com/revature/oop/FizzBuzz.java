48e52eeaa153b99df215e2285dbbab785ccb8afa
dimach44 <dimach44@gmail.com>
2019-09-05 08:47:58 -0400

    FizzBuzz

--- src/com/revature/oop/FizzBuzz.java
+++ src/com/revature/oop/FizzBuzz.java
@@ -4,9 +4,9 @@ public class FizzBuzz {
 	
 	public static void main(String[] args) {
 		for(int i = 0;i < 100;i++) {
-			if ((i + 1) / 3 == 1) {
+			if ((i + 1) % 3 == 0) {
 				System.out.println("Fizz");
-			} else if ((i + 1) / 5 == 1) {
+			} else if ((i + 1) % 5 == 0) {
 				System.out.println("Buzz");
 			} else {
 				System.out.println(i + 1);
