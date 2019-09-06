1422144374e3661e5d4da3e1d3970fa2a33e11b7
dimach44 <dimach44@gmail.com>
2019-09-06 08:33:42 -0400

    Collections

---
+++ src/com/revature/collections/UnderstandingCollections.java
@@ -0,0 +1,18 @@
+package com.revature.collections;
+
+import java.util.ArrayList;
+
+public class UnderstandingCollections {
+	public static void main(String[] args) {
+		ArrayList myList = new ArrayList();
+		myList.add(5);
+		myList.add("HelloWorld");
+		myList.add(new Object());
+		
+		ArrayList<Integer> myNums = new ArrayList<Integer>();
+		myNums.add(5);
+		//myNums.add("asd");
+		
+		
+	}
+}
