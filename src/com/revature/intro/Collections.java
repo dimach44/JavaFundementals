b984a17e4c8e7450526f700ac2338b6bbbe9c7cb
dimach <dimach44@gmail.com>
2019-09-05 20:18:59 -0400

    Maps

---
+++ src/com/revature/intro/Collections.java
@@ -0,0 +1,25 @@
+package com.revature.intro;
+
+import java.util.*;
+
+public class Collections {
+	
+	public static void main(String[] args) {
+		String[] arr = {"if", "it", "is", "to", "be", "it", "is", "up", "to", "me", "to", "delegate"};
+		map(arr);
+	}
+	
+	static void map(String[] args) {
+		Map<String, Integer> m = new HashMap<String, Integer>();
+
+        // Initialize frequency table from command line
+        for (String a : args) {
+            Integer freq = m.get(a);
+            m.put(a, (freq == null) ? 1 : freq + 1);
+        }
+
+        System.out.println(m.size() + " distinct words:");
+        System.out.println(m);
+	}
+	
+}
